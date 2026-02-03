a05aff561d8ebdda7c66e2ab7af4bf8711eecaa4
Sachin-Pathak-1 <sachinpathak9930@gmail.com>
2026-02-03 21:01:11 +0530

    Created Routes for all list pages

--- CMS-Front-end/src/App.jsx
+++ CMS-Front-end/src/App.jsx
@@ -7,9 +7,16 @@ import LandingPage from './pages/LandingPage/LandingPage.jsx';
 import FeaturesPage from './pages/LandingPage/FeaturesPage.jsx';
 import { TeacherListPage } from './pages/list/teacher/page.jsx';
 import { ParentListPage } from './pages/list/parent/page.jsx';
-import { Route,Routes } from 'react-router-dom';
-import './App.css'
 import { StudentListPage } from './pages/list/student/page.jsx';
+import { SubjectListPage } from './pages/list/subject/page.jsx';
+import { ClassesListPage } from './pages/list/classes/page.jsx';
+import { LessonsListPage } from './pages/list/lessons/page.jsx';
+import { ExamsListPage } from './pages/list/exams/page.jsx';
+import { AssignmentsListPage } from './pages/list/assignments/page.jsx';
+import { ResultsListPage } from './pages/list/results/page.jsx';
+import { EventsListPage } from './pages/list/events/page.jsx';
+import { AnnouncemetnsListPage } from './pages/list/announcements/page.jsx';
+import { TeacherDetails } from './pages/list/teacher/TeacherDetails.jsx';
 
 function App() {
   return (
@@ -23,6 +30,15 @@ function App() {
       <Route path="/list/teachers" element={<TeacherListPage />} />
       <Route path="/list/students" element={<StudentListPage />} />
       <Route path="/list/parents" element={<ParentListPage />} />
+      <Route path="/list/subjects" element={<SubjectListPage />} />
+      <Route path="/list/classes" element={<ClassesListPage />} />
+      <Route path="/list/lessons" element={<LessonsListPage />} />
+      <Route path="/list/exams" element={<ExamsListPage />} />
+      <Route path="/list/assignments" element={<AssignmentsListPage />} />
+      <Route path="/list/results" element={<ResultsListPage />} />
+      <Route path="/list/events" element={<EventsListPage />} />
+      <Route path="/list/announcements" element={<AnnouncemetnsListPage />} />
+      <Route path="/teacher/details" element={<TeacherDetails />} />
     </Routes>
   )
 }
